da1363d12b3b18a3ddf9e2505cd93226416ea39b
Algifarir <algifarir11112002@gmail.com>
2023-08-04 15:26:06 +0700

    First commit

--- Dockerfile
+++ Dockerfile
@@ -1,23 +1,43 @@
-FROM python:3.9-slim
+# Use the NVIDIA CUDA base image with GPU support
+FROM nvidia/cuda:11.0.3-base-ubuntu20.04
+
+# Install libGL and libglib2.0 libraries for Debian-based systems
+RUN apt-get update \
+    && apt-get install -y libgl1-mesa-glx libglib2.0-0
+
+# Install Python 3 and pip
+RUN apt-get install -y python3 python3-pip
 
 RUN groupadd -r user && useradd -m --no-log-init -r -g user user
+
+RUN mkdir -p /opt/app/models
+
+COPY ocelot400.h5 /opt/app/models/
 
 RUN mkdir -p /opt/app /input /output \
     && chown user:user /opt/app /input /output
 
 USER user
-WORKDIR /opt/app
+# Set the working directory inside the container
+WORKDIR /opt/app/
 
-ENV PATH="/home/user/.local/bin:${PATH}"
+# Set the PYTHONPATH environment variable
 ENV PYTHONPATH "${PYTHONPATH}:/opt/app/"
 
-RUN python -m pip install --user -U pip && python -m pip install --user pip-tools
+# Install pip-tools globally inside the container
+RUN python3 -m pip install pip-tools
 
+# Copy the contents of the current directory into the container working directory
 COPY ./ /opt/app/
 
-RUN pip install --no-cache-dir --upgrade pip \
-  && pip install --no-cache-dir -r requirements.txt
+# RUN pip install numpy opencv-python pandas tensorflow
+
+RUN pip3 install --no-cache-dir --upgrade pip \
+  && pip3 install --no-cache-dir -r requirements.txt \
+  && rm -rf /tmp/*
+
 
 COPY --chown=user:user process.py /opt/app/
 
 ENTRYPOINT [ "python", "-m", "process" ]
+
--- user/inference.py
+++ user/inference.py
@@ -1,5 +1,7 @@
 import numpy as np
-
+import cv2
+import pandas as pd
+import tensorflow as tf
 
 class Model():
     """
@@ -40,12 +42,28 @@ class Model():
         #############################################
 
         # The following is a dummy cell detection algorithm
-        prediction = np.copy(cell_patch[:, :, 2])
-        prediction[(cell_patch[:, :, 2] <= 40)] = 1
-        xs, ys = np.where(prediction.transpose() == 1)
-        class_id = [1] * len(xs) # Type of cell
-        probs = [1.0] * len(xs) # Confidence score
-
+        try:    
+            model = tf.keras.models.load_model('ocelot400.h5')
+            result = self.one_image_predictions(model, cell_patch)
+            result_inv = (255-result).astype(np.uint8)
+            ret, th = cv2.threshold(result_inv, 47, 255, cv2.THRESH_BINARY)
+            opening = cv2.morphologyEx(th, cv2.MORPH_OPEN, cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(11,11)))
+            dilation = cv2.dilate(opening, cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(7, 7)))
+            contours_res, _ = cv2.findContours(dilation, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+            if contours_res:
+                center_points = []
+                for contour in contours_res:
+                    M = cv2.moments(contour)
+                    
+                    if M["m00"] != 0:
+                        center_x = int(M["m10"] / M["m00"])
+                        center_y = int(M["m01"] / M["m00"])
+                        center_points.append((center_x, center_y, 2, 1.0))
+                return center_points
+            else :
+                return []
+        except:
+            return []
         #############################################
         ####### RETURN RESULS PER SAMPLE ############
         #############################################
@@ -55,4 +73,68 @@ class Model():
         # - int: cell's y-coordinate in the cell patch
         # - int: class id of the cell, either 1 (BC) or 2 (TC)
         # - float: confidence score of the predicted cell
-        return list(zip(xs, ys, class_id, probs))
+        
+    
+
+    def HistEqual(self, image):
+        ycbcr = cv2.cvtColor(image, cv2.COLOR_BGR2YCrCb)
+        y, cb, cr = cv2.split(ycbcr)
+        hist = cv2.equalizeHist(y)
+        merged = cv2.merge([hist, cb, cr])
+        res = cv2.cvtColor(merged, cv2.COLOR_YCrCb2BGR)
+        return res
+
+
+    def crop_image(image, patch_size):
+        image_height, image_width, channels = image.shape
+        patch_height, patch_width = patch_size
+
+        num_patches_vertical = image_height // patch_height
+        num_patches_horizontal = image_width // patch_width
+
+        patches = image[:num_patches_vertical * patch_height, :num_patches_horizontal * patch_width, :]
+        patches = np.split(patches, num_patches_vertical, axis=0)
+        patches = [np.split(row_patches, num_patches_horizontal, axis=1) for row_patches in patches]
+        patches = np.array(patches).reshape(-1, patch_height, patch_width, channels)
+
+        return patches
+
+
+    def parse_image(self, image_path):
+        image_equal = self.HistEqual(image_path)
+        image_equal = cv2.cvtColor(image_equal, cv2.COLOR_BGR2RGB)
+        image_equal = image_equal/255
+        result =self.crop_image(image_equal, (128, 128))
+        return result
+
+    def concat_patch(image_pred_cont):
+        num_patches = len(image_pred_cont)
+
+        num_patches_per_row = int(np.sqrt(num_patches))
+        num_patches_per_col = num_patches // num_patches_per_row
+
+        concatenated_image = np.concatenate([np.concatenate(image_pred_cont[i*num_patches_per_col:(i+1)*num_patches_per_col], axis=1) for i in range(num_patches_per_row)], axis=0)
+
+        return concatenated_image
+    
+    '''
+    Make a code to receive 1 image with size 1024 x 1024
+    make it into 128 x 128 so it will be 16 patch images
+    detection using a pretrained model
+    make 16 patch images into 1 image 1024 x 1024
+    Show it
+    '''
+
+    def one_image_predictions(self,pretrained_model, image_path, mask_path=None):
+        image_patch = self.parse_image(image_path)
+        image_pred_cont = []
+        for one_patch_image in image_patch:
+            temp = np.expand_dims(one_patch_image, axis=0)
+            predictions = pretrained_model.predict(temp)
+            # preds_sigmoid = keras.activations.sigmoid(predictions)
+            predicted_result = np.squeeze(predictions, axis=0)
+            gray_image = predicted_result[:, :, 0]
+            normalized_image = (gray_image * 255).astype(np.uint8)
+            image_pred_cont.append(normalized_image)
+        result = self.concat_patch(image_pred_cont)
+        return result
